d074f87778b64c178367df728e757b4a6192dd8a
Dtnexo <pt22ugm@eduvaud.ch>
2025-12-13 20:11:52 +0100

    feat(code) udpate database

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,15 +1,29 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
+let sequelize;
+
+if (process.env.MYSQL_URL) {
+  sequelize = new Sequelize(process.env.MYSQL_URL, {
     dialect: "mysql",
     logging: false,
-  }
-);
+  });
+} else if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "mysql",
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: "mysql",
+      logging: false,
+    }
+  );
+}
 
 module.exports = sequelize;
